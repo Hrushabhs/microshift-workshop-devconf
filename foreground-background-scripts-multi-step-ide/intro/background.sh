c79da14a0f7c06220cc9c9c39e06917b3219253f
Hrushabh Sirsulwar <Hrushabhs@gmail.com>
2026-01-03 20:16:59 +0530

    Update background.sh

--- foreground-background-scripts-multi-step-ide/intro/background.sh
+++ foreground-background-scripts-multi-step-ide/intro/background.sh
@@ -1,4 +1,27 @@
-set -x # to test stderr output in /var/log/killercoda
-echo starting... # to test stdout output in /var/log/killercoda
-sleep 3
-echo done > /tmp/background0
+#!/bin/bash
+# 1. Install required packages
+apt-get update && apt-get install -y skopeo git
+
+# 2. Start Local Registry
+podman run -d -p 5000:5000 --restart always --name workshop-registry registry:2
+
+# 3. Clone your workshop repo to /root/workshop
+git clone https://github.com/hsirsulw/airgapped-microshift-deployment-centos.git /root/workshop
+cd /root/workshop
+
+# 4. Mirror images using your list to localhost:5000
+REGISTRY="localhost:5000"
+while read -r IMAGE; do
+    [[ -z "$IMAGE" || "$IMAGE" =~ ^# ]] && continue
+    echo "Mirroring $IMAGE..."
+    DEST_PATH="${IMAGE#*/}"
+    skopeo copy --all --preserve-digests --format v2s2 --dest-tls-verify=false \
+        docker://"$IMAGE" docker://"$REGISTRY/$DEST_PATH"
+done < image-list.txt
+
+# 5. Pre-pull base images to host cache for speed
+podman pull quay.io/centos-bootc/centos-bootc:stream9
+podman pull quay.io/centos-bootc/centos-bootc:stream10
+
+# Signal completion
+touch /tmp/finished
