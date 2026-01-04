a133d4528ff27b596700e46da0b08c909f940b2b
Hrushabh Sirsulwar <Hrushabhs@gmail.com>
2026-01-04 18:55:15 +0530

    Update background.sh

--- foreground-background-scripts/background.sh
+++ foreground-background-scripts/background.sh
@@ -1,9 +1,71 @@
 #!/bin/bash
+set -eux
 
-set -x # to test stderr output in /var/log/killercoda
+#####################################
+# CONTROLPLANE SETUP
+#####################################
 
-echo starting... # to test stdout output in /var/log/killercoda
+apt-get update
+apt-get install -y podman skopeo git
 
-sleep 5 # some long running background task
+# Start local registry
+podman run -d \
+  --name workshop-registry \
+  --restart always \
+  -p 5000:5000 \
+  registry:2
+
+echo "✅ Registry running on controlplane:5000"
+
+#####################################
+# MIRROR IMAGES (controlplane)
+#####################################
+
+git clone https://github.com/hsirsulw/airgapped-microshift-deployment-centos.git /root/workshop
+cd /root/workshop
+
+REGISTRY="localhost:5000"
+
+while read -r IMAGE; do
+    [[ -z "$IMAGE" || "$IMAGE" =~ ^# ]] && continue
+    DEST_PATH="${IMAGE#*/}"
+    # The --format v2s2 flag fixes the Nginx 500 error
+    skopeo copy --all --preserve-digests --format v2s2 --dest-tls-verify=false \
+        docker://"$IMAGE" docker://"localhost:5000/$DEST_PATH"
+done < image-list.txt
+
+#####################################
+
+#####################################
+# NODE01 SETUP
+#####################################
+
+ssh root@node01 <<'EOF'
+set -eux
+
+# Install required tools
+apt-get update
+apt-get install -y podman git skopeo
+
+# Clone workshop repo
+git clone https://github.com/hsirsulw/airgapped-microshift-deployment-centos.git /root/workshop
+cd /root/workshop
+
+# Ensure registry config dir exists
+mkdir -p /etc/containers/registries.conf.d
+
+# Replace mirror IP with controlplane hostname
+sed -i 's/192.168.100.1/controlplane/g' assets/99-offline.conf
+
+# Copy offline registry config
+cp assets/99-offline.conf /etc/containers/registries.conf.d/99-offline.conf
+
+echo "✅ node01 configured to use controlplane:5000 as registry mirror"
+EOF
+
+#####################################
+# SIGNAL READY
+#####################################
 
 touch /tmp/finished
+echo "🎉 Workshop environment ready"
